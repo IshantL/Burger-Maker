08e5d0916fef248ddc7ac2802ef9bd5ce8b9ede1
Ishant Lambhate <Ishant.Lambhate@allocatesoftware.com>
2019-09-09 15:39:38 +0530

    updating logic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,6 +3,7 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  const ingredient
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
@@ -12,11 +11,7 @@ class BurgerIngredient extends Component {
                 ingredient = <div className={classes.BreadBottom}></div>;
                 break;
             case ( 'bread-top' ):
-                ingredient = (
-                    <div className={classes.BreadTop}>
-                        
-                    </div>
-                );
+                ingredient =<div className={classes.BreadTop}></div>;
                 break;
             case ( 'meat' ):
                 ingredient = <div className={classes.Meat}></div>;

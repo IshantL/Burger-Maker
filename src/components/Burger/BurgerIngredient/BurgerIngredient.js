e877eea7c961af02186ef0ee11fcd57a85385f2c
IshantL <ishantlambhate@gmail.com>
2019-09-10 22:33:55 +0530

    adjusted css change

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -11,7 +11,10 @@ class BurgerIngredient extends Component {
                 ingredient = <div className={classes.BreadBottom}></div>;
                 break;
             case ( 'bread-top' ):
-                ingredient =<div className={classes.BreadTop}></div>;
+                ingredient =( <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>)
                 break;
             case ( 'meat' ):
                 ingredient = <div className={classes.Meat}></div>;

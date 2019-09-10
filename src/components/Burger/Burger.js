0a41320b74f883a2e74e3e09cf72985e8a63a3ce
IshantL <ishantlambhate@gmail.com>
2019-09-10 23:26:48 +0530

    iterator

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,19 +3,22 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  // const transformedIngredients = Object.keys(props.ingredients)
-  // .map(igKey =>{
-
-  // });
-  const ingredients = (a,b) =>{
-    let ingredientList =[];
-    for (let i =0; i<=b; i++){
-      ingredientList.push(<BurgerIngredient type={a} />) 
-    }
-    return ingredientList;
-  }
-  const transformedIngredients = Object.entries(props.ingredients)
-        .map(item => ingredients(...item));
+  const transformedIngredients = Object.keys(props.ingredients)
+  .map( igKey => {
+    return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+    } );
+} )
+  //other logic
+  // const ingredients = (a,b) =>{
+  //   let ingredientList =[];
+  //   for (let i =0; i<=b; i++){
+  //     ingredientList.push(<BurgerIngredient type={a} />) 
+  //   }
+  //   return ingredientList;
+  // }
+  // const transformedIngredients = Object.entries(props.ingredients)
+  //       .map(item => ingredients(...item));
 
  
   return (

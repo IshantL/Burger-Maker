6c62e56a01787ed78951ca19183936067fb5a1ef
IshantL <ishantlambhate@gmail.com>
2019-09-11 11:08:35 +0530

    reduce to map it into 1 array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  debugger;
   const transformedIngredients = Object.keys(props.ingredients)
   .map( igKey => {
     return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
     } );
 } )
+.reduce((arr,el) =>{
+    return arr.concat(el)
+},[])
   //other logic
   // const ingredients = (a,b) =>{
   //   let ingredientList =[];

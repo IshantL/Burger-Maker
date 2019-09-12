0390ce1ce918d58ea445d5075aec731137357f6a
IshantL <ishantlambhate@gmail.com>
2019-09-12 10:41:25 +0530

    modify state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,10 +10,10 @@ class BurgerBuilder extends Component {
     // }
     state = {
         ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
+            salad: 2,
+            bacon: 1,
+            cheese: 2,
+            meat: 1
         }
     }
 

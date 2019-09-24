fbf4169accc4c98b802e0be63f53c1f6a54d53bd
IshantL <ishantlambhate@gmail.com>
2019-09-24 15:43:57 +0530

    added Order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from 
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -77,6 +78,7 @@ class BurgerBuilder extends Component {
         // {salad: true, meat: false, ...}
         return (
             <>
+            <Modal/>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
